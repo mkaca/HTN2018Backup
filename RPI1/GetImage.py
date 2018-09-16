2c87f444972defd7f404c258f7fce98778d54ebc
mkaca <dabestineuro@gmail.com>
2018-09-16 06:47:20 +0000

    added hardcoded synchronous activity

--- RPI1/GetImage.py
+++ RPI1/GetImage.py
@@ -5,21 +5,38 @@ import socket
 from PIL import Image
 import numpy
 
+print("starting code")
 camera = PiCamera()
 camera.resolution = (240,240)
-camera.start_preview()
-time.sleep(1)
-camera.capture('/home/pi/Desktop/HTN2018/RPI1/TestImg.jpg')
-camera.stop_preview()
-time.sleep(2)
-imgData = Image.open('/home/pi/Desktop/HTN2018/RPI1/TestImg.jpg')
-imgData = (numpy.array(imgData))
-print (imgData)
-print(numpy.shape(imgData))
-
 s = socket.socket()
 port = 12452
 s.settimeout(8)
+modulusBoi  = 12
+
+check = True
+while(True):
+  if (int(str(datetime.datetime.now())[17:19]) % modulusBoi == 0):
+    print ("starting timeCheck")
+    break
+time.sleep(0.1)
+while (check):
+  timeS = int(str(datetime.datetime.now())[21:24])
+  print((timeS))
+  timeB = int(str(datetime.datetime.now())[17:19]) 
+  print(timeB)
+  if (timeS == 0 and timeB % modulusBoi == 0):
+      check = False
+      print('breaking')
+
+camera.start_preview()
+time.sleep(0.4)
+camera.capture('/home/pi/Desktop/HTN2018/RPI1/TestImg.jpg')
+camera.stop_preview()
+time.sleep(0.4)
+imgData = Image.open('/home/pi/Desktop/HTN2018/RPI1/TestImg.jpg')
+imgData = (numpy.array(imgData))
+#print (imgData)
+#print(numpy.shape(imgData))
 
 s.connect(("169.254.244.191",port))
 strImgData = imgData.tostring()
--- RPI2/GetImageR.py
+++ RPI2/GetImageR.py
@@ -4,27 +4,45 @@ import time
 import socket
 from PIL import Image
 import numpy
+import datetime
 
+print("starting code")
 camera = PiCamera()
 camera.resolution = (240,240)
-camera.start_preview()
-time.sleep(1)
-camera.capture('/home/pi/Desktop/HTN2018/RPI2/TestImgR.jpg')
-camera.stop_preview()
-time.sleep(2)
-imgData = Image.open('/home/pi/Desktop/HTN2018/RPI2/TestImgR.jpg')
-imgData = (numpy.array(imgData))
-print (imgData)
-print(numpy.shape(imgData))
-
 s = socket.socket()
 port = 12553
 s.settimeout(8)
+modulusBoi = 12
+
+check = True
+while(True):
+  if (int(str(datetime.datetime.now())[17:19]) % modulusBoi == 0):
+    print ("starting timeCheck")
+    break
+time.sleep(0.1)
+while (check):
+  timeS = int(str(datetime.datetime.now())[21:24])
+  print((timeS))
+  timeB = int(str(datetime.datetime.now())[17:19]) 
+  print(timeB)
+  if (timeS == 0 and timeB % modulusBoi == 0):
+      check = False
+      print('breaking')
+
+camera.start_preview()
+time.sleep(0.4)
+camera.capture('/home/pi/Desktop/HTN2018/RPI2/TestImgR.jpg')
+camera.stop_preview()
+time.sleep(0.4)
+imgData = Image.open('/home/pi/Desktop/HTN2018/RPI2/TestImgR.jpg')
+imgData = (numpy.array(imgData))
+#print (imgData)
+#print(numpy.shape(imgData))
 
 s.connect(("169.254.250.64",port))
 strImgData = imgData.tostring()
 bufferFlex = numpy.frombuffer(strImgData, dtype = numpy.uint8, count = -1)
-print(numpy.shape(bufferFlex))
+#print(numpy.shape(bufferFlex))
 print(" ")
 print("done connecting R")
 
