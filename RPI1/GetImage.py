558bce1db1d8d48b7c88841e89a815705aee6431
mkaca <dabestineuro@gmail.com>
2018-09-16 04:43:39 +0100

    updated

--- RPI1/GetImage.py
+++ RPI1/GetImage.py
@@ -1,3 +1,4 @@
+import json
 from picamera import PiCamera
 import time
 import socket
@@ -5,24 +6,29 @@ from PIL import Image
 import numpy
 
 camera = PiCamera()
-
+camera.resolution = (240,240)
 camera.start_preview()
 time.sleep(1)
 camera.capture('/home/pi/Desktop/HTN2018/RPI1/TestImg.jpg')
 camera.stop_preview()
 time.sleep(2)
 imgData = Image.open('/home/pi/Desktop/HTN2018/RPI1/TestImg.jpg')
-imgData = numpy.array(imgData)
+imgData = (numpy.array(imgData))
 print (imgData)
+print(numpy.shape(imgData))
+
 
 s = socket.socket()
-port = 12456
-s.settimeout(5)
+port = 12452
+s.settimeout(8)
 
 s.connect(("169.254.244.191",port))
-strImgData = str(imgData)
+strImgData = imgData.tostring()
+bufferFlex = numpy.frombuffer(strImgData, dtype = numpy.uint8, count = -1)
+print(numpy.shape(bufferFlex))
+print(" ")
 print("done connecting")
 s.sendall(strImgData)
 
-print(s.recv(1024))
+print(s.recv(2048))
 s.close
